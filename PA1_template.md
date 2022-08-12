348c881106fd14e762d45af3f562242425d9f193
nrich22 <nar5501@rit.edu>
2022-08-12 13:23:38 -0400

    finish project 1

--- PA1_template.md
+++ PA1_template.md
@@ -10,7 +10,8 @@ output:
 
 
 ## Loading and preprocessing the data
-```{r}
+
+```r
 library(ggplot2)
 library(dplyr)
 library(lattice)
@@ -20,7 +21,8 @@ data <- read.csv("activity.csv")
 
 
 ## What is mean total number of steps taken per day?
-```{r}
+
+```r
 totalSteps <- data %>% group_by(date) %>% summarise(dailySteps = sum(steps, na.rm = TRUE))
 png("plot1.png", width=480, height=480, units="px", bg="white")
 ggplot(totalSteps, aes(dailySteps)) + geom_histogram(binwidth = 2000) +
@@ -31,12 +33,24 @@ stepsMean <- mean(totalSteps$dailySteps, na.rm=TRUE)
 stepsMedian <- median(totalSteps$dailySteps, na.rm=TRUE)
 
 message(stepsMean)
+```
+
+```
+## 9354.22950819672
+```
+
+```r
 message(stepsMedian)
+```
+
+```
+## 10395
 ```
 
 
 ## What is the average daily activity pattern?
-```{r}
+
+```r
 intervalSteps <- data %>% 
   group_by(interval) %>%
   summarise(steps = mean(steps, na.rm =TRUE))
@@ -48,9 +62,14 @@ ggplot(data=intervalSteps, aes(x=interval, y=steps)) +
 message(835)
 ```
 
+```
+## 835
+```
+
 
 ## Imputing missing values
-```{r}
+
+```r
 missingData <- !complete.cases(data)
 imputedData <- data %>%
   mutate(
@@ -72,16 +91,34 @@ meanDifference <- inmputedMean - stepsMean
 medianDifference <- imputedMedian - stepsMedian
 
 message(meanDifference)
+```
+
+```
+## 1411.95917104856
+```
+
+```r
 message(meanDifference)
+```
+
+```
+## 1411.95917104856
 ```
 
 
 ## Are there differences in activity patterns between weekdays and weekends?
-```{r}
+
+```r
 weekendDays <- c("Saturday", "Sunday")
 imputedData$dow = as.factor(ifelse(is.element(weekdays(as.Date(imputedData$date)), weekendDays), "Weekend", "Weekday"))
 imputerSteps <- aggregate(steps ~ interval + dow, imputedData, inmputedMean)
+```
 
+```
+## Error in get(as.character(FUN), mode = "function", envir = envir): object 'FUN' of mode 'function' was not found
+```
+
+```r
 png("plot4.png", width=480, height=480, units="px", bg="white")
 xyplot(imputedData$steps ~ imputedData$interval|imputedData$dow, main="Average Steps per Day by Interval", xlab="Interval", ylab="Steps", layout=c(1,2), type="l")
 ```
--- course_project1.R
+++ course_project1.R
@@ -1,26 +1,15 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-author: "Nicholas Ricciarelli"
-date: "August 12, 2022"
-output: 
-  html_document:
-    number_sections: true
-    keep_md: true
----
+# Nicholas Ricciarelli
+# Reproducible Research
+# Course Project 1
 
-
-## Loading and preprocessing the data
-```{r}
 library(ggplot2)
 library(dplyr)
 library(lattice)
 
+# read data
 data <- read.csv("activity.csv")
-```
 
-
-## What is mean total number of steps taken per day?
-```{r}
+# What is mean total number of steps taken per day?
 totalSteps <- data %>% group_by(date) %>% summarise(dailySteps = sum(steps, na.rm = TRUE))
 png("plot1.png", width=480, height=480, units="px", bg="white")
 ggplot(totalSteps, aes(dailySteps)) + geom_histogram(binwidth = 2000) +
@@ -32,11 +21,8 @@ stepsMedian <- median(totalSteps$dailySteps, na.rm=TRUE)
 
 message(stepsMean)
 message(stepsMedian)
-```
 
-
-## What is the average daily activity pattern?
-```{r}
+# What is the average daily activity pattern?
 intervalSteps <- data %>% 
   group_by(interval) %>%
   summarise(steps = mean(steps, na.rm =TRUE))
@@ -45,12 +31,8 @@ ggplot(data=intervalSteps, aes(x=interval, y=steps)) +
     geom_line() +
     xlab("5-minute intervals") +
     ylab("Average number of steps taken")
-message(835)
-```
 
-
-## Imputing missing values
-```{r}
+# Imputing missing values
 missingData <- !complete.cases(data)
 imputedData <- data %>%
   mutate(
@@ -73,15 +55,13 @@ medianDifference <- imputedMedian - stepsMedian
 
 message(meanDifference)
 message(meanDifference)
-```
 
-
-## Are there differences in activity patterns between weekdays and weekends?
-```{r}
+# Are there differences in activity patterns between weekdays and weekends?
 weekendDays <- c("Saturday", "Sunday")
 imputedData$dow = as.factor(ifelse(is.element(weekdays(as.Date(imputedData$date)), weekendDays), "Weekend", "Weekday"))
 imputerSteps <- aggregate(steps ~ interval + dow, imputedData, inmputedMean)
 
 png("plot4.png", width=480, height=480, units="px", bg="white")
 xyplot(imputedData$steps ~ imputedData$interval|imputedData$dow, main="Average Steps per Day by Interval", xlab="Interval", ylab="Steps", layout=c(1,2), type="l")
-```
+dev.off()
+
